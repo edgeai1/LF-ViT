53ef01386de7b906c4fa53d1de79097e9dbab5e5
edgeai1 <104889295+edgeai1@users.noreply.github.com>
2023-12-18 09:11:28 +0800

    Update models_deit.py

--- deit/models_deit.py
+++ deit/models_deit.py
@@ -311,7 +311,7 @@ class CFVisionTransformer(nn.Module):
 
 
 @register_model
-def cf_deit_small(pretrained=False, **kwargs):
+def lf_deit_small(pretrained=False, **kwargs):
     model = CFVisionTransformer(
         patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
